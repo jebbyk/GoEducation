dc45ce7b66c794353cd4d8fd7318ce4b5751bc4d
jebbyk <zaicev9797@gmail.com>
2021-12-30 12:19:57 +0600

    some fixes into analizing norm distribution

--- mathMethods/compute/misc/SliceUtils.go
+++ mathMethods/compute/misc/SliceUtils.go
@@ -1,21 +1,25 @@
 package misc
 
-func FindMinFloat(values []float64) float64 {
+func FindMinFloat(values []float64) int {
 	min := 999999999999999.9 //TODO something with it later but now im lazy :P
+	id := 0
 	for i := 0; i < len(values); i++ {
 		if values[i] < min {
 			min = values[i]
+			id = i
 		}
 	}
-	return min
+	return id
 }
 
-func FindMaxFloat(values []float64) float64 {
+func FindMaxFloat(values []float64) int {
 	max := -99999999999999.9
+	id := 0
 	for i := 0; i < len(values); i++ {
 		if values[i] > max {
 			max = values[i]
+			id = i
 		}
 	}
-	return max
+	return id
 }
--- mathMethods/main.go
+++ mathMethods/main.go
@@ -11,9 +11,9 @@ import (
 	. "mathMethods/visualize/graphs"
 )
 
-func countProbabilities(values []float64, stepsAmount int) []float64 {
-	min := misc.FindMinFloat(values)
-	max := misc.FindMaxFloat(values)
+func countProbabilities(values []float64, stepsAmount int, verbose bool) []float64 {
+	min := values[misc.FindMinFloat(values)]
+	max := values[misc.FindMaxFloat(values)]
 
 	rng := max - min
 
@@ -36,6 +36,13 @@ func countProbabilities(values []float64, stepsAmount int) []float64 {
 
 		probabilities = append(probabilities, probability)
 
+	}
+
+	if verbose {
+		maxProbId := misc.FindMaxFloat(probabilities)
+		l := float64(maxProbId) * stepLength
+		r := l + stepLength
+		fmt.Println("MinNumber: ", min, ". MostFrequentPart: ", l, "-", r, ". MaxNumber: ", max, ". MostFreqPartProb: ", probabilities[maxProbId])
 	}
 
 	return probabilities
@@ -71,7 +78,6 @@ func generateExpDistribution(exponent float64, iterations int, generator *random
 		value := -exponent * math.Log10(generator.Fgen())
 		values = append(values, value)
 	}
-
 	return values
 }
 func calcEstimatedExpProbabilities(min float64, max float64, stepsAmount int) []float64 {
@@ -90,15 +96,15 @@ func calcEstimatedExpProbabilities(min float64, max float64, stepsAmount int) []
 	return probabilities
 }
 
-func generateNormDistribution(Q float64, iterations int, generator *random.Generator) []float64 {
+func generateNormDistribution(Q float64, A float64, iterations int, generator *random.Generator) []float64 {
 	values := []float64{}
 	for i := 0; i < iterations/2; i++ {
 
 		u0 := generator.Fgen()
 		u1 := generator.Fgen()
 
-		v0 := u0*2.0 - 1.0
-		v1 := u1*2.0 - 1.0
+		v0 := (u0 * 2.0) - 1.0
+		v1 := (u1 * 2.0) - 1.0
 
 		S := v0*v0 + v1*v1
 
@@ -110,20 +116,22 @@ func generateNormDistribution(Q float64, iterations int, generator *random.Gener
 		x0 := v0 * math.Sqrt((-2.0*math.Log10(S))/S)
 		x1 := v1 * math.Sqrt((-2.0*math.Log10(S))/S)
 
-		x0 = x0 * Q
-		x1 = x1 * Q
+		x0 = x0*Q + A
+		x1 = x1*Q + A
 
 		values = append(values, x0, x1)
 	}
 	return values
 }
-func idkHowToNameThisShit(value float64) float64 {
-	// bunch of thing is dropped from original formula cuz anyway a = 0 and q = 1
-	exp := math.Exp(-0.5 * (value * value)) // https://ru.wikipedia.org/wiki/%D0%9D%D0%BE%D1%80%D0%BC%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5_%D1%80%D0%B0%D1%81%D0%BF%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5
-	fract := 1.0 / math.Sqrt(2.0*math.Pi)
+func normDistributionProbDensity(Q float64, A float64, value float64) float64 {
+	expFract0 := (value - A) * (value - A)
+	expFract1 := 2 * (Q * Q)
+	exp := math.Exp(-expFract0 / expFract1) // https://ru.wikipedia.org/wiki/%D0%9D%D0%BE%D1%80%D0%BC%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5_%D1%80%D0%B0%D1%81%D0%BF%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5
+	fract := 1.0 / (Q * math.Sqrt(2.0*math.Pi))
+
 	return fract * exp
 }
-func calcEstimatedNormProbabilities(min float64, max float64, stepsAmount int) []float64 {
+func calcEstimatedNormProbabilities(Q float64, A float64, min float64, max float64, stepsAmount int) []float64 {
 	rng := max - min
 	stepLength := rng / float64(stepsAmount)
 
@@ -133,10 +141,16 @@ func calcEstimatedNormProbabilities(min float64, max float64, stepsAmount int) [
 		lft := min + float64(i)*stepLength
 		rgt := lft + stepLength
 
-		prob := idkHowToNameThisShit(((lft - rgt) / rgt) * (lft - rgt))
+		prob := normDistributionProbDensity(Q, A, rgt) * stepLength
 
 		probabilities = append(probabilities, prob)
 	}
+
+	//maxProbId := misc.FindMaxFloat(probabilities)
+	//l := float64(maxProbId) * stepLength
+	//r := l + stepLength
+	//fmt.Println("MinNumber: ", min, ". MostFrequentPart: ", l, "-", r, ". MaxNumber: ", max, ". MostFreqPartProb: ", probabilities[maxProbId])
+
 	return probabilities
 }
 
@@ -156,49 +170,48 @@ func pearsonCriterion(estimated []float64, actual []float64) float64 {
 	return result
 }
 
-func displayValues(values []float64, buffer *Buffer, posX float64, posY float64, scaleX float64, scaleY float64, steps int) {
-
-	probabilities := countProbabilities(values, steps)
+func displayValues(values []float64, buffer *Buffer, posX float64, posY float64, scaleX float64, scaleY float64) {
 	graphPos := new(Vector2).Set(posX, posY)
 	graphScale := new(Vector2).Set(scaleX, scaleY)
 	graph := new(Graph).Init(graphPos, graphScale, STYLE_BARS)
-	graph.Draw(probabilities, buffer)
-
+	graph.Draw(values, buffer)
 }
 
 func main() {
 	generator := new(random.Generator).Init(0)
 	prompter := new(Prompter).Init("\n", "Incorrect input!")
-	buffer := new(Buffer).Init(24, 160)
+	buffer := new(Buffer).Init(24, 200)
 
-	iterations := prompter.RequestInteger("Enter iterations amount: ")
-	//iterations := 10
+	numberAmount := prompter.RequestInteger("Enter numbers amount: ")
+	//numberAmount := 10
 	exponent := prompter.RequestFloat("Enter exponent grater than 0: ")
-	Q := prompter.RequestFloat("Enter Q between grater than 0: ")
+	Q := prompter.RequestFloat("Enter Q  grater than 0: ")
+	A := prompter.RequestFloat("Enter A: ")
 
-	norm := []float64{}
-
-	//for iterations := 2; iterations < 1000000; iterations += 2 { // add 2 in each iteration cuz normDistribution generates 2 numbers at once
-	even := generateEvenDistribution(iterations, generator)
-	estimatedEvenProbabilities := calcEstimatedEvenProbabilities(misc.FindMinFloat(even), misc.FindMaxFloat(even), 20)
-	actualEvenProbabilities := countProbabilities(even, 20)
+	even := generateEvenDistribution(numberAmount, generator)
+	estimatedEvenProbabilities := calcEstimatedEvenProbabilities(even[misc.FindMinFloat(even)], even[misc.FindMaxFloat(even)], 20)
+	actualEvenProbabilities := countProbabilities(even, 20, false)
 	evenPearsonCriterion := pearsonCriterion(estimatedEvenProbabilities, actualEvenProbabilities)
-	displayValues(even, buffer, 0.0, 0.0, 0.3, 8.0, 100)
+	//displayValues(estimatedEvenProbabilities, buffer, 0.0, 0.0, 0.3, 8.0)
+	displayValues(actualEvenProbabilities, buffer, 0.0, 0.0, 0.3, 8.0)
 
-	exp := generateExpDistribution(exponent, iterations, generator)
-	estimatedExpProbabilities := calcEstimatedExpProbabilities(misc.FindMinFloat(exp), misc.FindMaxFloat(exp), 20)
-	actualExpProbabilities := countProbabilities(exp, 20)
+	exp := generateExpDistribution(exponent, numberAmount, generator)
+	estimatedExpProbabilities := calcEstimatedExpProbabilities(exp[misc.FindMinFloat(exp)], exp[misc.FindMaxFloat(exp)], 20)
+	actualExpProbabilities := countProbabilities(exp, 20, false)
 	expPearsonCriterion := pearsonCriterion(estimatedExpProbabilities, actualExpProbabilities)
-	displayValues(exp, buffer, 0.35, 0.0, 0.3, 8.0, 100)
+	//displayValues(estimatedExpProbabilities, buffer, 0.0, 0.0, 0.3, 2.0)
+	displayValues(actualExpProbabilities, buffer, 0.35, 0.0, 0.3, 2.0)
 
-	norm = generateNormDistribution(Q, iterations, generator)
-	for i := 0; i < iterations; i++ {
-		norm[i] += 3.0 //Offset them so they can be displayed on my shitty graph :)
-	}
-	estimatedNormProbabilities := calcEstimatedNormProbabilities(misc.FindMinFloat(norm), misc.FindMaxFloat(norm), 20)
-	actualNormProbabilities := countProbabilities(norm, 20)
+	//norm := []float64{}
+	norm := generateNormDistribution(Q, A, numberAmount, generator)
+	//for i := 0; i < numberAmount; i++ {
+	//	norm[i] += 3.0 //Offset them so they can be displayed on my shitty graph :)
+	//}
+	estimatedNormProbabilities := calcEstimatedNormProbabilities(Q, A, norm[misc.FindMinFloat(norm)], norm[misc.FindMaxFloat(norm)], 20)
+	actualNormProbabilities := countProbabilities(norm, 20, false)
 	normPearsonCriterion := pearsonCriterion(estimatedNormProbabilities, actualNormProbabilities)
-	displayValues(norm, buffer, 0.7, 0.0, 0.3, 8.0, 100)
+	//displayValues(estimatedNormProbabilities, buffer, 0.35, 0.0, 0.3, 4.0)
+	displayValues(actualNormProbabilities, buffer, 0.7, 0.0, 0.3, 4.0)
 
 	fmt.Println("=================================================================================================================================================================================================")
 	buffer.Print()
@@ -209,7 +222,4 @@ func main() {
 	fmt.Println("exp: ", expPearsonCriterion)
 	fmt.Println("norm: ", normPearsonCriterion)
 	buffer.Clear()
-
-	//time.Sleep(100 * time.Millisecond)
-	//}
 }
